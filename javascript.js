bee6f9490485cecc2a5c21728dcde1499c944477
Eric <liyangzhong@hotmail.com>
2019-09-25 23:49:23 +0800

    second commit

--- javascript.js
+++ javascript.js
@@ -899,5 +899,14 @@ console.log(increment);
 
 // console.log(getMaxOfTmrw(LOCAL_FORECAST));
 //===========
-const [z,x] = [1,2,3,4,5,6];
-console.log(z,x);
+// const [z,x,,y] = [1,2,3,4,5,6];
+// console.log(z,x, y);
+
+// let a =8, b = 6;
+// (() => {
+//     "use strict";
+//     [a,b]=[b,a]
+// })();
+// console.log(a);
+// console.log(b);
+//=======================
